58c20ccbe673e3121f3c970b759ce5f608757a45
Gautam Manuel <gautam.manuel@gmail.com>
2022-06-15 11:44:10 +0530

    Scaffolded auth page

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,18 +1,74 @@
-import React from 'react'
-import { Paper, Stack, Tabs, TextInput, Text } from '@mantine/core';
+import React, {useEffect, useState} from 'react'
+import { Paper, Stack, Tabs, TextInput, Button, Box } from '@mantine/core';
+import { useForm } from '@mantine/form'
 
 export default function Auth() {
-  return (
-    <Tabs position='center' variant="pills">
-      <Tabs.Tab label="Sign In">
-          <Stack>
-            <TextInput
-                label="EMAIL OR USERNAME"
-                required
-            />
-          </Stack>
-      </Tabs.Tab>
-      <Tabs.Tab label="Sign Up">Second tab content</Tabs.Tab>
-    </Tabs>
-  )
+
+    const [activeTab, setActiveTab] = useState(1);
+
+    const form = activeTab === "signIn" ? useForm({
+        initialValues: {
+            email : 0,
+            password: ""
+        }
+    }) :
+    useForm({
+        initialValues: {
+            email : 0,
+            password: ""
+        }
+    })
+
+    const handleSignin = (values) => {
+        console.log("login")
+    }
+
+    const handleSignUp = (values) => {
+        console.log("signupo")
+    }
+
+    const onChange = (active, tabKey) => {
+        setActiveTab(active);
+    };
+
+    return (
+        <Box sx={{width: "90%", maxWidth: '350px', margin: 'auto'}}>
+            <Tabs active={activeTab} onTabChange={onChange} grow position='center' variant="pills">
+                <Tabs.Tab label="Sign In" tabKey="signIn">
+                    <form style={{padding: '0 8px'}} onSubmit={form.onSubmit((values) => handleSignin(values))}>
+                        <Stack>
+                            <TextInput
+                                label="EMAIL OR USERNAME"
+                                required
+                                {...form.getInputProps('email')}
+                            />
+                            <TextInput
+                                label="PASSWORD"
+                                required
+                                {...form.getInputProps('password')}
+                            />
+                            <Button type='submit'>Log In</Button>
+                        </Stack>
+                    </form>
+                </Tabs.Tab>
+                <Tabs.Tab label="Sign Up" tabKey="signUp">
+                    <form style={{padding: '0 8px'}} onSubmit={form.onSubmit((values) => handleSignUp(values))}>
+                        <Stack>
+                            <TextInput
+                                label="EMAIL OR USERNAME"
+                                required
+                                {...form.getInputProps('email')}
+                            />
+                            <TextInput
+                                label="PASSWORD"
+                                required
+                                {...form.getInputProps('password')}
+                            />
+                            <Button type='submit'>Log In</Button>
+                        </Stack>
+                    </form>
+                </Tabs.Tab>
+                </Tabs>
+        </Box>
+    )
 }
