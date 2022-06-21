e2ff602ead21f3c07970118ef75099641cfd74aa
Gautam Manuel <gautam.manuel@gmail.com>
2022-06-21 14:42:53 +0530

    Fixed Auth Page

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,12 +1,15 @@
 import React, {useEffect, useState, useContext} from 'react'
 import { z } from 'zod'
-import { Paper, Stack, Tabs, TextInput, Button, Box } from '@mantine/core';
+import { Paper, Stack, Tabs, TextInput, Button, Box, useMantineColorScheme } from '@mantine/core';
+import LogoDark from "../assets/logo-dark.svg"
+import LogoLight from "../assets/logo-light.svg"
 import { useForm, zodResolver } from '@mantine/form'
 import { AuthContext } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
 export default function Auth() {
     const { signIn, signUp, authed, loading } = useContext(AuthContext);
+    const colorScheme = useMantineColorScheme()
     const navigate = useNavigate()
 
     const [activeTab, setActiveTab] = useState(0);
@@ -59,54 +62,57 @@ export default function Auth() {
 
     return (
         <Box sx={{width: "90%", maxWidth: '350px', margin: 'auto'}}>
-            <Tabs active={activeTab} onTabChange={onChange} grow position='center' variant="pills">
-                <Tabs.Tab label="Sign In" tabKey="signIn">
-                    <form onSubmit={signInForm.onSubmit((values) => handleSignIn(values))}>
-                        <Stack>
-                            <TextInput
-                                label="EMAIL OR USERNAME"
-                                required
-                                {...signInForm.getInputProps('email')}
-                            />
-                            <TextInput
-                                label="PASSWORD"
-                                type='password'
-                                required
-                                {...signInForm.getInputProps('password')}
-                            />
-                            {!authed && <Button loading={loading} type='submit' mt={16}>Log In</Button>}
-                        </Stack>
-                    </form>
-                </Tabs.Tab>
-                <Tabs.Tab label="Sign Up" tabKey="signUp">
-                    <form onSubmit={signUpForm.onSubmit((values) => handleSignUp(values))}>
-                        <Stack>
-                            <TextInput
-                                label="FULL NAME"
-                                required
-                                {...signUpForm.getInputProps('name')}
-                            />
-                            <TextInput
-                                label="EMAIL"
-                                required
-                                {...signUpForm.getInputProps('email')}
-                            />
-                            <TextInput
-                                label="Username"
-                                required
-                                {...signUpForm.getInputProps('username')}
-                            />
-                            <TextInput
-                                label="PASSWORD"
-                                required
-                                {...signUpForm.getInputProps('password')}
-                            />
-                            {!authed && <Button loading={loading} type='submit' mt={16}>Sign Up</Button>}
-                        </Stack>
-                    </form>
-                </Tabs.Tab>
-            </Tabs>
-            {authed && <Button mt={16} fullWidth onClick={handleOpenAppClick}>Open App</Button>}
+            <Stack>
+                <img style={{padding: '24px', maxHeight: '80px'}} src={colorScheme !== 'dark' ? LogoLight : LogoDark} alt="" />
+                <Tabs active={activeTab} onTabChange={onChange} grow position='center' variant="pills">
+                    <Tabs.Tab label="Sign In" tabKey="signIn">
+                        <form onSubmit={signInForm.onSubmit((values) => handleSignIn(values))}>
+                            <Stack>
+                                <TextInput
+                                    label="EMAIL OR USERNAME"
+                                    required
+                                    {...signInForm.getInputProps('email')}
+                                />
+                                <TextInput
+                                    label="PASSWORD"
+                                    type='password'
+                                    required
+                                    {...signInForm.getInputProps('password')}
+                                />
+                                {!authed && <Button loading={loading} type='submit' mt={16}>Log In</Button>}
+                            </Stack>
+                        </form>
+                    </Tabs.Tab>
+                    <Tabs.Tab label="Sign Up" tabKey="signUp">
+                        <form onSubmit={signUpForm.onSubmit((values) => handleSignUp(values))}>
+                            <Stack>
+                                <TextInput
+                                    label="FULL NAME"
+                                    required
+                                    {...signUpForm.getInputProps('name')}
+                                />
+                                <TextInput
+                                    label="EMAIL"
+                                    required
+                                    {...signUpForm.getInputProps('email')}
+                                />
+                                <TextInput
+                                    label="Username"
+                                    required
+                                    {...signUpForm.getInputProps('username')}
+                                />
+                                <TextInput
+                                    label="PASSWORD"
+                                    required
+                                    {...signUpForm.getInputProps('password')}
+                                />
+                                {!authed && <Button loading={loading} type='submit' mt={16}>Sign Up</Button>}
+                            </Stack>
+                        </form>
+                    </Tabs.Tab>
+                </Tabs>
+                {authed && <Button mt={16} fullWidth onClick={handleOpenAppClick}>Open App</Button>}
+            </Stack>
         </Box>
     )
 }
