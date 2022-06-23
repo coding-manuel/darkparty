0c93d3b9dd9ae963fb36860b1d6b4ee2dadb9ae2
Gautam Manuel <gautam.manuel@gmail.com>
2022-06-23 11:10:09 +0530

    Added styles to messages

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Tooltip, Textarea, Text, Group, Stack } from '@mantine/core';
+import { Button, Tooltip, Box, Textarea, Text, Group, Stack, useMantineTheme } from '@mantine/core';
 import LogoLight from '../assets/logo-light.svg'
 import { ArrowFatRight, Link } from 'phosphor-react';
 import { showNotification } from '@mantine/notifications';
@@ -11,6 +11,16 @@ const StyledTooltip = ({children, label}) => {
             {children}
         </Tooltip>
 )}
+
+const StyledMessage = ({me, message}) => {
+    const theme = useMantineTheme()
+
+    return(
+    <Box ml={me && 24} mr={!me && 24} sx={{width: 'fit-content', border: '1px solid #fff', borderRadius: 4, padding: 8, background: me ? theme.colors.dark[6] : theme.colors.orange[5]}}>
+        <Text size='sm' color='white'>{message}</Text>
+    </Box>
+    )
+}
 
 const ChatBox = () => {
     const handleShareLink = () => {
@@ -29,67 +39,30 @@ const ChatBox = () => {
                     <Link onClick={handleShareLink} cursor='pointer' size={16} weight="fill" />
                 </StyledTooltip>
             </Group>
-            <Stack sx={{flexGrow: 2, height: '80%', overflow: 'scroll'}}>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
-                <div>
-                    asfjdsakfgj
-                </div>
+            <Stack spacing={8} sx={{flexGrow: 2, height: '80%', overflow: 'scroll', padding: '4px 8px'}}>
+                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                <StyledMessage me={false} message='Yes me too' />
+                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                <StyledMessage me={false} message='Yes me too' />
+                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                <StyledMessage me={false} message='Yes me too' />
+                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                <StyledMessage me={false} message='Yes me too' />
+                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                <StyledMessage me={false} message='Yes me too' />
+                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                <StyledMessage me={false} message='Yes me too' />
+                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                <StyledMessage me={false} message='Yes me too' />
+                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                <StyledMessage me={false} message='Yes me too' />
+                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                <StyledMessage me={false} message='Yes me too' />
+                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                <StyledMessage me={false} message='Yes me too' />
             </Stack>
             <Stack spacing={0} sx={{flexDirection: 'row'}}>
                 <Textarea
