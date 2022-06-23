1f8a7615af7b15a3c5f44ab09e5d385951ffe371
Gautam Manuel <gautam.manuel@gmail.com>
2022-06-23 13:52:45 +0530

    Made chat closable

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Button, Tooltip, Box, Textarea, Text, Group, Stack, useMantineTheme } from '@mantine/core';
+import React, {useState} from 'react';
+import { Button, Tooltip, Box, Textarea, Text, Group, Stack, useMantineTheme, ActionIcon } from '@mantine/core';
 import LogoLight from '../assets/logo-light.svg'
-import { ArrowFatRight, Link } from 'phosphor-react';
+import { ArrowFatLeft, ArrowFatRight, Link } from 'phosphor-react';
 import { showNotification } from '@mantine/notifications';
 import { notificationStyles } from '../globalStyles';
+import { PlayerContext } from '../contexts/PlayerContext';
+import { useContext } from 'react';
 
 const StyledTooltip = ({children, label}) => {
     return(
@@ -17,12 +19,15 @@ const StyledMessage = ({me, message}) => {
 
     return(
     <Box ml={me && 24} mr={!me && 24} sx={{width: 'fit-content', border: '1px solid #fff', borderRadius: 4, padding: 8, background: me ? theme.colors.dark[6] : theme.colors.orange[5]}}>
-        <Text size='sm' color='white'>{message}</Text>
+        <Text size='xs' color='white'>{message}</Text>
     </Box>
     )
 }
 
 const ChatBox = () => {
+    const [chatOpen, setChatOpen] = useState(true);
+    const {controlVisible} = useContext(PlayerContext);
+
     const handleShareLink = () => {
         navigator.clipboard.writeText('https://djghsdjhgjdsgjh')
         showNotification({
@@ -32,49 +37,54 @@ const ChatBox = () => {
     }
 
     return (
-        <Stack spacing={0} sx={{height: '100%', width: 300}}>
-            <Group spacing={110} position='center' py={12} sx={{borderBottom: '2px solid #ffffff'}}>
-                <img src={LogoLight} alt="" />
-                <StyledTooltip label='Share Link'>
-                    <Link onClick={handleShareLink} cursor='pointer' size={16} weight="fill" />
-                </StyledTooltip>
-            </Group>
-            <Stack spacing={8} sx={{flexGrow: 2, height: '80%', overflow: 'scroll', padding: '4px 8px'}}>
-                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
-                <StyledMessage me={false} message='Yes me too' />
-                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
-                <StyledMessage me={false} message='Yes me too' />
-                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
-                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
-                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
-                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
-                <StyledMessage me={false} message='Yes me too' />
-                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
-                <StyledMessage me={false} message='Yes me too' />
-                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
-                <StyledMessage me={false} message='Yes me too' />
-                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
-                <StyledMessage me={false} message='Yes me too' />
-                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
-                <StyledMessage me={false} message='Yes me too' />
-                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
-                <StyledMessage me={false} message='Yes me too' />
-                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
-                <StyledMessage me={false} message='Yes me too' />
-                <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
-                <StyledMessage me={false} message='Yes me too' />
+        <>
+            <ActionIcon onClick={() => setChatOpen(!chatOpen)} sx={{position: 'absolute', top: 40, right: chatOpen ? 300 : 0, transition: 'right .3s ease-out, opacity .2s ease-out', opacity: controlVisible ? 1 : 0}}>
+                <ArrowFatRight size={16} weight="fill" style={{transform : chatOpen && 'rotate(180deg)', transition: '.2s ease-out'}} />
+            </ActionIcon>
+            <Stack spacing={0} sx={{height: '100%', width: chatOpen ? 300 : 0, opacity: chatOpen ? 1 : 0, transition: 'width .3s ease-out, opacity .2s ease-out'}}>
+                <Group spacing={150} position='center' py={12} sx={{borderBottom: '2px solid #ffffff'}}>
+                    <img src={LogoLight} alt="" />
+                    <StyledTooltip label='Share Link'>
+                        {chatOpen && <Link onClick={handleShareLink} cursor='pointer' size={16} weight="fill" />}
+                    </StyledTooltip>
+                </Group>
+                <Stack spacing={8} sx={{flexGrow: 2, height: '80%', overflow: 'scroll', padding: '4px 8px'}}>
+                    <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                    <StyledMessage me={false} message='Yes me too' />
+                    <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                    <StyledMessage me={false} message='Yes me too' />
+                    <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                    <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                    <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                    <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                    <StyledMessage me={false} message='Yes me too' />
+                    <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                    <StyledMessage me={false} message='Yes me too' />
+                    <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                    <StyledMessage me={false} message='Yes me too' />
+                    <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                    <StyledMessage me={false} message='Yes me too' />
+                    <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                    <StyledMessage me={false} message='Yes me too' />
+                    <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                    <StyledMessage me={false} message='Yes me too' />
+                    <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                    <StyledMessage me={false} message='Yes me too' />
+                    <StyledMessage me={true} message='Dude, I cannot believe what just happened' />
+                    <StyledMessage me={false} message='Yes me too' />
+                </Stack>
+                <Stack spacing={0} sx={{flexDirection: 'row'}}>
+                    <Textarea
+                        sx={{justifySelf: 'flex-end', border: '1px solid #ffffff', borderWidth: '1px 1px 1px 0', padding:'0 8px', width: '100%'}}
+                        maxRows={1}
+                        placeholder="Send Text"
+                        variant="unstyled"
+                        required
+                    />
+                    <Button radius={0}><ArrowFatRight size={16} weight="fill" /></Button>
+                </Stack>
             </Stack>
-            <Stack spacing={0} sx={{flexDirection: 'row'}}>
-                <Textarea
-                    sx={{justifySelf: 'flex-end', border: '1px solid #ffffff', borderWidth: '1px 1px 1px 0', padding:'0 8px', width: '100%'}}
-                    maxRows={1}
-                    placeholder="Send Text"
-                    variant="unstyled"
-                    required
-                />
-                <Button radius={0}><ArrowFatRight size={16} weight="fill" /></Button>
-            </Stack>
-        </Stack>
+        </>
     );
 }
 
